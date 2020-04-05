58764c3b78603b65da245d149a1367b64160eff7
David  Melo <david.melo10@hotmail.com>
2020-04-05 19:04:23 -0300

    Configuração projeto

---
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -0,0 +1,9 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CervejasController {
+
+}
