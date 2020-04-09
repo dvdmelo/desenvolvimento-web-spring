ff23dde77811687b5c27d98f978dd3d9291ea4f5
David  Melo <david.melo10@hotmail.com>
2020-04-09 06:58:48 -0300

    Inclusão de novas paginas

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,8 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,9 +16,11 @@ import com.algaworks.brewer.model.Cerveja;
 @Controller
 public class CervejasController {
 
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+	
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {	
-		
+	
 		return "cerveja/CadastroCerveja";
 	}
 	
